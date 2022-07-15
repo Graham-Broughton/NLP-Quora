01d153d6abda8eb57cd1e3c6a71029fa8405bc09
root <broughton.graham@gmail.com>
2022-07-15 00:44:22 +0000

    finished baseline logistic model and running XGBoost

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import tensorflow as tf
+import pickle
 
 def set_seed(seed):
     """
@@ -9,3 +10,6 @@ def set_seed(seed):
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
     tf.random.set_seed(seed)
+
+def get_pickle(feature):
+    return pickle.load(open(f'drive/MyDrive/Quora_Duplicate_Questions/src/{feature}.pkl', 'rb'))
